68b5ac350f39137c7897afdc39476b6c499ec4b9
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-22 17:11:38 +0000

    code for webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the area of a triangle
+ */
+function myButtonClicked() {
+  // input
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById("area").innerHTML = "Area is " + area + "cm²"
+}
